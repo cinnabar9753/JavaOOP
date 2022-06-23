3dc53cd4ec3dc290977e844e9d71d6b73e4a7875
cinnabar <1225715474@qq.com>
2022-06-23 18:56:42 +0800

    Initial commit

---
+++ src/main/java/Student.java
@@ -0,0 +1,58 @@
+public class Student {
+    private String name;
+    private int age;
+    private String gender;
+    private int ID;
+
+    public Student(String name, int age, String gender, int ID) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.ID = ID;
+    }
+
+    public Student() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", gender='" + gender + '\'' +
+                ", ID=" + ID +
+                '}';
+    }
+}
